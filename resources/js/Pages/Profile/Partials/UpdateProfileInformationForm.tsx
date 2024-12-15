4354c2e6529c846c5cd3e2d875e45b8d430cbffa
alkadoHs <alkadosichone@gmail.com>
2024-12-15 10:03:11 +0300

    maintainance welcome page

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.tsx
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.tsx
@@ -51,7 +51,6 @@ export default function UpdateProfileInformation({
                         value={data.name}
                         onChange={(e) => setData('name', e.target.value)}
                         required
-                        isFocused
                         autoComplete="name"
                     />
 
