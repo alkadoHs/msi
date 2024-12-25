9d6ecf4990dda79ec95a8a285c71126cbd2be3e8
alkadoHs <alkadosichone@gmail.com>
2024-12-25 16:29:28 +0300

    branch sales download excel

--- app/Http/Controllers/Reports/BranchSalesReportController.php
+++ app/Http/Controllers/Reports/BranchSalesReportController.php
@@ -9,6 +9,7 @@ use Inertia\Inertia;
 use Maatwebsite\Excel\Facades\Excel;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\View;
 
 class BranchSalesReportController extends Controller
 {
@@ -28,14 +29,32 @@ class BranchSalesReportController extends Controller
     public function exportExcel(Request $request)
     {
         [$startDate, $endDate] = $this->getDates($request);
+
         $reportData = $this->getQuery($startDate, $endDate);
 
-        return Excel::download(new \App\Exports\BranchSalesReportExport($reportData), 'branch_sales_report.xlsx');
+        $fileName = 'branch_sales_report_' . now()->format('Ymd_His') . '.xlsx';
+
+        return Excel::download(new class($reportData) implements \Maatwebsite\Excel\Concerns\FromView {
+            public $reportData;
+
+            public function __construct($reportData)
+            {
+                $this->reportData = $reportData;
+            }
+
+            public function view(): \Illuminate\Contracts\View\View
+            {
+                return View::make('reports.branch_sales_excel', [
+                    'branches' => $this->reportData
+                ]);
+            }
+        }, $fileName);
     }
 
     public function exportPdf(Request $request)
     {
         [$startDate, $endDate] = $this->getDates($request);
+
         $reportData = $this->getQuery($startDate, $endDate);
 
         $pdf = Pdf::loadView('pdf.branch-sales-report', [
@@ -58,18 +77,17 @@ class BranchSalesReportController extends Controller
     protected function getQuery(Carbon $startDate, Carbon $endDate)
     {
         return Branch::where('company_id', auth()->user()->company_id)
-        ->withSum(['expenses as expenses' => function ($query) use($startDate, $endDate) {
-            return $query->whereBetween('expenses.created_at', [$startDate, $endDate]);
-        }], 'cost')
-        ->withSum(['orderItems as sales' => function ($query) use($startDate, $endDate) {
+            ->withSum(['expenses as expenses' => function ($query) use ($startDate, $endDate) {
+                return $query->whereBetween('expenses.created_at', [$startDate, $endDate]);
+            }], 'cost')
+            ->withSum(['orderItems as sales' => function ($query) use ($startDate, $endDate) {
                 return $query->whereBetween('order_items.created_at', [$startDate, $endDate])
-                             ->whereRelation('order', 'status', 'paid');
+                    ->whereRelation('order', 'status', 'paid');
             }], 'total')
-        ->withSum(['orderItems as profit' => function ($query) use($startDate, $endDate) {
-            return $query->whereBetween('order_items.created_at', [$startDate, $endDate])
-                         ->whereRelation('order', 'status', 'paid');
-        }], 'profit')
-        ->get();
+            ->withSum(['orderItems as profit' => function ($query) use ($startDate, $endDate) {
+                return $query->whereBetween('order_items.created_at', [$startDate, $endDate])
+                    ->whereRelation('order', 'status', 'paid');
+            }], 'profit')
+            ->get();
     }
-    
 }
