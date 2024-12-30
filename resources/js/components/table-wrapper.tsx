36ab784e14a8a4abf54dad0e7b66253861a34771
alkadoHs <alkadosichone@gmail.com>
2024-12-30 20:10:35 +0300

    ...

--- resources/js/components/table-wrapper.tsx
+++ resources/js/components/table-wrapper.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 
 const TableWrapper = ({ children }: { children: ReactNode }) => {
     return (
-        <div className="mx-auto max-w-full px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-full px-3 sm:px-4 lg:px-6">
             <div className="w-full  border border-gray-200 rounded-xl overflow-hidden bg-white shadow-sm  p-0 text-gray-900 dark:text-gray-100 sm:rounded-lg dark:bg-gray-800 dark:border-gray-700">
                 {children}
             </div>
